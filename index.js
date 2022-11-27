303a6af24c528d099248615bbd94c98d3514c3de
@j_kahuha <johntexavir@gmail.com>
2022-11-27 12:58:20 +0300

    atlas connection

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-var express=require("express");
-var cool=require("cool-ascii-faces");
-var app=express(),
-    Router=express.Router();
+var express = require('express');
+var cool = require('cool-ascii-faces');
+const { establishConnection } = require('./utils');
+
+require('dotenv').config();
+
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT;
+
+var app = express(),
+  Router = express.Router();
 
 app.use(express.static('public'));
-app.use(function(req,res,next){
-    res.header('Access-Control-Allow-Origin','http://localhost:7770');
-    res.header('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept');
-    res.header('Access-Control-Allow-Credentials',true);
-    next();
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:7770');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept'
+  );
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
 });
-Router.get('/',(req,res)=>{
-    res.status(200).send('<b>Hey There!!!</b> <hr/>Welcome');
+Router.get('/', async (req, res) => {
+  const mongo = await establishConnection(MONGO_URI);
+  res.status(200).send(`<b>Hey There!!!</b> <hr/>Welcome. <i>${mongo}</i>`);
 });
-Router.get('/coolFaces',(req,res)=>{
-    res.status(200).send(cool());
+Router.get('/coolFaces', (req, res) => {
+  res.status(200).send(cool());
 });
-app.use('/',Router);
-app.listen(5250,()=>{
-    console.log('App listening Port 5250');
+app.use('/', Router);
+app.listen(PORT, () => {
+  console.log(`App listening Port ${PORT}`);
 });
-
